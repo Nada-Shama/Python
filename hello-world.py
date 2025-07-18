3fb604c03beba0bf467d59e2a4b6e1ce4e5fd181
Nada-Shama <nada.h.shamaa@gmail.com>
2025-07-18 07:48:12 +0300

    Create hello-world.py

---
+++ hello-world.py
@@ -0,0 +1,15 @@
+print("Hello World")
+
+name = "nada"
+print("Hello",name,"!")
+
+print("Hello"+name+"!")
+
+num = '55'
+print("Hello"+num+"!")
+
+food1 = 'pasta'
+food2 = 'pizza'
+print("I love to eat",food1,'and',food2)
+
+
